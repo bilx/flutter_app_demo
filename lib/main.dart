8abf1c52933350ae19594194963a1af4eccfcfbb
blix <18310048353@163.com>
2020-09-11 20:01:08 +0800

    添加首页page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_app_antrebate/core/service/net/repository/app_repository.dart';
+import 'package:flutter_app_antrebate/ui/main/page/index_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,40 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            AppRepository.homeIndex();
-          },
-          child: Text("请求数据"),
+        title: '百姓生活+',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.pink,
         ),
-      ),
-    );
+        home: IndexPage());
   }
 }
--- lib/ui/main/page/index_page.dart
+++ lib/ui/main/page/index_page.dart
@@ -1,36 +1,49 @@
-import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_app_antrebate/common/resouce/images.dart';
 import 'package:flutter_app_antrebate/common/resouce/strings.dart';
-
+import 'package:flutter_app_antrebate/ui/ant/page/ant_home_page.dart';
+import 'package:flutter_app_antrebate/ui/home/page/home_page.dart';
+import 'package:flutter_app_antrebate/ui/user/page/personal_home_page.dart';
 
 class IndexPage extends StatelessWidget {
-
-
   List<BottomNavigationBarItem> navigationList = [
     BottomNavigationBarItem(
         icon: Image.asset(Images.tabbar_home_default),
         activeIcon: Image.asset(Images.tabbar_home_selected),
-        title: Text(Strings.tabbar_home_text)
-    ),
+        title: Text(Strings.tabbar_home_text)),
     BottomNavigationBarItem(
         icon: Image.asset(Images.tabbar_ziying_default),
         activeIcon: Image.asset(Images.tabbar_ziying_selected),
-        title: Text(Strings.tabbar_ant_text)
-    ),
+        title: Text(Strings.tabbar_ant_text)),
     BottomNavigationBarItem(
         icon: Image.asset(Images.tabbar_me_default),
         activeIcon: Image.asset(Images.tabbar_me_selected),
-        title: Text(Strings.tabbar_me_text)
-    )
+        title: Text(Strings.tabbar_me_text))
   ];
 
+  final List<Widget> tabBodies = [
+    HomePage(),
+    AntHomePage(),
+    PersonalHomePage()
+  ];
+
+  var currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: navigationList,
+        onTap: (index) {
+
+        },
+      ),
+      body: IndexedStack(index: currentIndex, children: tabBodies),
     );
   }
 }
